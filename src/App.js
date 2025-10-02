18724857b6d4656cdafecb9fab4642adb55bc6d3
FerNyXnX <fernyxnx@gmail.com>
2025-10-02 14:37:47 -0700

    Feat(app): Integrar aplicación completa
    - Conectar todos los componentes en App.js
    >> - Configurar Provider de Redux en index.js
    >> - Agregar footer con información de copyright
    >> - Implementar layout responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import ShoppingCart from './components/ShoppingCart';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <main>
+        <ProductList />
+        <div className="border-t-2 border-gray-200 mt-8"></div>
+        <ShoppingCart />
+      </main>
+      <footer className="bg-gray-800 text-white text-center p-4 mt-12">
+        <p>&copy; 2025 Mi Tienda Online. Todos los derechos reservados.</p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
