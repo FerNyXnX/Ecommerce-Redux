49e44232aa83e72e5caa78806bef87954acffeeb
FerNyXnX <fernyxnx@gmail.com>
2025-10-09 14:32:21 -0700

    Refactor(store): Migrar store a TypeScript
    - Tipar configuración del store
    - Crear hooks tipados (useAppDispatch, useAppSelector)
    - Exportar tipos RootState y AppDispatch
    - Configurar middleware con tipos

---
+++ src/app/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productsReducer from '../features/products/productsSlice';
+import cartReducer from '../features/cart/cartSlice';
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['products/fetchProducts/fulfilled']
+      }
+    })
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
